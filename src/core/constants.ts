b0c29bd376a27348deaa09dbd0cf56dc6e8e0b62
Jan Czekala <jan.czekala99@web.de>
2022-07-02 16:27:32 +0200

    feat: update instagram App version

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,6 +1,6 @@
-export const APP_VERSION = '121.0.0.29.119';
-export const APP_VERSION_CODE = '185203708';
-export const SIGNATURE_KEY = '9193488027538fd3450b83b7d05286d4ca9599a0f7eeed90d8c85925698a05dc';
+export const APP_VERSION = '222.0.0.13.114';
+export const APP_VERSION_CODE = '350696709';
+export const SIGNATURE_KEY = '388ece79ebc0e70e87873505ed1b0ff335ae2868a978cc951b6721c41d46a30a';
 export const BREADCRUMB_KEY = 'iN4$aGr0m';
 export const SIGNATURE_VERSION = '4';
 export const EXPERIMENTS =
